07660d14fb929e745935a067aacfa4b8de8f608d
Дарья Коннова <dashustic@yandex.ru>
2023-04-14 16:01:15 +0300

    correct secret key

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,18 +1,3 @@
-"""yatube URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -31,6 +16,9 @@ handler404 = 'core.views.page_not_found'
 handler500 = "core.views.server_error"
 
 if settings.DEBUG:
+    import debug_toolbar
+    
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
